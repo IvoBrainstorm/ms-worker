c9d013c6a8cc8c93b30427b219491381c1e37317
Ivo Matavele <mataveleivo@gmail.com>
2022-03-02 16:37:53 +0200

    Project hr-worker created

---
+++ hr-worker/src/test/java/mz/co/matavele/hrworker/HrWorkerApplicationTests.java
@@ -0,0 +1,13 @@
+package mz.co.matavele.hrworker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HrWorkerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
