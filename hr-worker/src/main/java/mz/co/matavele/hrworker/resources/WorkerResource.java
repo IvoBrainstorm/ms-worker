b61c8315d9acbb1378446413333a3c36a2d13e83
Ivo Matavele <mataveleivo@gmail.com>
2022-04-04 11:46:27 +0200

    hr-worker as a config server client

--- hr-worker/src/main/java/mz/co/matavele/hrworker/resources/WorkerResource.java
+++ hr-worker/src/main/java/mz/co/matavele/hrworker/resources/WorkerResource.java
@@ -3,6 +3,7 @@ package mz.co.matavele.hrworker.resources;
 import mz.co.matavele.hrworker.entities.Worker;
 import mz.co.matavele.hrworker.repositories.WorkerRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -15,6 +16,8 @@ import java.util.List;
 @RequestMapping(value = "/worker")
 public class WorkerResource {
 
+    @Value("${test.config}")
+    private String testConfig;
 
     @Autowired
     private WorkerRepository repository;
@@ -23,6 +26,12 @@ public class WorkerResource {
     public ResponseEntity<List<Worker>> findAll(){
         List<Worker> workerList = repository.findAll();
         return ResponseEntity.ok(workerList);
+    }
+
+    @GetMapping(value = "/config")
+    public ResponseEntity<Void> getConfigs(){
+        System.out.println("CONFIG " + testConfig);
+        return ResponseEntity.noContent().build();
     }
 
     @GetMapping(value = "/{id}")
