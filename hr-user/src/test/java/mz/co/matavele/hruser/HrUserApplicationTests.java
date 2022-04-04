737af74f748e317c33d5b5273be9e2fd98f7095e
Ivo Matavele <mataveleivo@gmail.com>
2022-04-04 21:51:05 +0200

    Hr user create

---
+++ hr-user/src/test/java/mz/co/matavele/hruser/HrUserApplicationTests.java
@@ -0,0 +1,13 @@
+package mz.co.matavele.hruser;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HrUserApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
